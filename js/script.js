0db7d52ff639ef31bf714f6333d611da8879ba7d
Farjana Akter <85429034+Farjana79@users.noreply.github.com>
2022-07-20 18:47:34 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// // js function
+
+
+// imageclick
+let bigImg = document.querySelector('.product_large_img img')
+function showImg(pic){
+  bigImg.src = pic;
+}
+// imageclick
+
+
+
+//jq function
+  $(document).ready(function(){
+ 
+  
+});
+//jq function
